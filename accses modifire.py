dd4879e09e24e1ab52850d60e8f3d43545f3d8d4
PT8573 <priyatiwari96166@gmail.com>
2026-01-09 14:48:21 +0530

    Add files via upload

---
+++ accses modifire.py
@@ -0,0 +1,141 @@
+# # #public accses
+# # class aipa:
+         
+# #  def __init__ (self,name):
+# #         self.name=name
+# #  def display(self):
+# #         print(self.name)
+# # s=aipa("komal")
+# # s.display()
+# # print(s.name)
+
+# # #private accss
+# # class aipa:
+# #      def __init__(self,name,tools):
+# #           self.name=name
+# #           self._tools=tools
+# #      def use (self):
+# #           print(self._tools)
+# #           print(self.name)
+# # t=aipa("shivali","almira")
+# # t.use ()
+# # print(t._tools)
+
+
+
+
+
+
+# # class aipa:
+# #      def __init__(self,name,tools,register):
+# #            self.name=name
+# #            self._tools=tools
+# #            self.__register=register
+# #      def classregister (self):
+# #           print(self._tools)
+# # r=aipa("shreya","laptop","cr")
+# # print(r.name,r._tools,r._aipa__register)
+
+
+# # class studentrecord:
+# #      def __init__(self,name,roll_no,marks):
+# #           self.name=name
+# #           self._roll_no=roll_no
+# #           self._marks=marks
+# #      def display (self):
+# #           print(self.name)
+# #           print(self._roll_no)
+# #           print(self._marks)
+# # p=studentrecord("shivali",14,98)
+# # p.display()
+# # print(p.name,p._roll_no,p._marks)
+
+#     #inharitence
+# # class grandfather:
+# #  def house (self):
+# #     print(" this is grand father house")
+
+# # class father(grandfather):
+# #     def car (self):
+# #         print("this is father car")
+
+# # class son(father):
+# #     def bike (self):
+# #         print("this is sons car")
+
+# # abc=father()
+# # # abc.bike()
+# # abc.car()
+# # abc.house()
+
+# # xyz=son()
+# # xyz.bike()
+# # xyz.car()
+# # xyz.house()
+
+# # g=grandfather()
+# # g.house()
+# # # g.car()
+# # # g.bike()
+
+
+
+
+# # class nsti:
+# #     def campus(self):
+# #         print("this is nsti campus")
+# # class trade(nsti):
+# #      def teacher(self):
+# #          print("these are 40 staff")
+# # class std(trade):
+# #     def student(self):
+# #         print("these are 650 student")
+
+# # c=nsti()
+# # c.campus()
+
+
+# # t=trade()
+
+# # t.teacher
+
+
+# # s=std()
+# # s.campus()
+# # s.student()
+# # s.teacher()
+
+# class milk:
+#     def liquid(self):
+#         print("this is a milk")
+# class curd(milk):
+#     def solid(liquid):
+#         print("this is solid fome of milk")
+# class buttermilk(curd):
+#     def micture(solid):
+#         print("this is a muxture of milk and curd")
+
+# # m=milk()
+# # m.liquid()
+# # # m.solid()
+# # # m.micture()
+
+# c=curd()
+# c.liquid()
+# c.solid()
+# # c.micture()
+
+# # bm=buttermilk()
+# # bm.liquid()
+# # bm.solid()
+# # bm.micture()
+
+
+
+
+
+
+
+
+        
+
