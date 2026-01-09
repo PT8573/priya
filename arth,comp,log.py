45b682301cf1df618b93db0f763ef6ec087906f9
PT8573 <priyatiwari96166@gmail.com>
2026-01-09 14:47:59 +0530

    Add files via upload

---
+++ arth,comp,log.py
@@ -0,0 +1,48 @@
+#arthmatic oprator in python
+# x=10
+# y=5
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x**y)
+# print(x/y)
+# print(x//y)
+# print(x%y)
+
+#comperative oprators
+# x=10
+# y=5
+# print(x>y)
+# print(x<y)
+# print(x==y)
+# print(x>=y)
+# print(x<=y)
+# print(x!=y)
+
+#logical opretors [and,or,not(revers)])
+# a=5
+# b=6
+# c=7
+# print(a<b and c>b)
+# print(a>b or c>a)
+# print(not c>a)
+# print(c>a)
+
+#assinment oprators
+# x=10
+# y=5
+# x+=y
+# print(x)
+# x-=y
+# print(x)
+# x*=y
+# print(x)
+# x/=y
+# print(x)
+# x%=y
+# print(x)
+# x**=y
+# print(x)
+# x//=y
+# print(x)
+
